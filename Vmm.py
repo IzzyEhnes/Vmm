a8abc4c52c5fa3097d71211a3bc71174e80c8705
Izzy Ehnes <izzyehnes@gmail.com>
2022-03-30 14:52:30 -0700

    Split lines into lists

--- Vmm.py
+++ Vmm.py
@@ -1,6 +1,13 @@
-def read_file():
+def process_file():
     with open('inputs/input1.txt','r') as f:
         contents = f.readlines()
-    print(contents)
 
-read_file()
+    lines = list()
+
+    for line in contents:
+        line = " ".join(line.split()).split()
+        lines.append(line)
+
+    print(lines)
+
+process_file()
