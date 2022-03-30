a1396d391c9642139254910b649113941ddfb777
Izzy Ehnes <izzyehnes@gmail.com>
2022-03-30 16:24:40 -0700

    Add Vmm class

--- Vmm.py
+++ Vmm.py
@@ -1,17 +1,36 @@
-def process_file():
-    with open('inputs/input1.txt','r') as f:
-        contents = f.read()
+import constants
+
+class Vmm:
+
+    PD = [None] * constants.PD_SIZE
+    PT = [None] * constants.PT_SIZE
+    P = [None] * constants.PAGE_SIZE
+    
+    def process_file():
+        with open('inputs/input1.txt','r') as f:
+            contents = f.read()
+            
+        line = " ".join(contents.split()).split()
+
+        input = list()
+
+        for element in line:
+            if element == 'p' or element == 'r':
+                input.append(line[:2])
+                line = line[2:]
+            elif element == 'w':
+                input.append(line[:3])
+                line = line[3:]
         
-    line = " ".join(contents.split()).split()
+        for i in input:
+            print(i)
+        
+        print(len(input))
 
-    input = list()
+        return input
 
-    for element in line:
-        if element == 'p' or element == 'r':
-            input.append(line[:2])
-            line = line[2:]
-        elif element == 'w':
-            input.append(line[:3])
-            line = line[3:]
+    def run_Vmm(memory_accesses):
+        available_page_frames = memory_accesses[0][1]
 
-process_file()
+memory_accesses = Vmm.process_file()
+Vmm.run_Vmm(memory_accesses)
